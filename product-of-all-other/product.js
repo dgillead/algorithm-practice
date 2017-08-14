940dfeb1b97843f9453aaf87c20bceb292ac1edb
Darnell Gillead <dlgillead@gmail.com>
2017-08-14 17:24:54 -0500

    re-factored to reduce time complexity

--- product-of-all-other/product.js
+++ product-of-all-other/product.js
@@ -1,29 +1,16 @@
-// For each element in the array
-// Multiply every other number together except for the number at the current index
-// Push each value onto an array that's not the current value
-// Multiply all values contained in that array together and push onto return array
-// Move on to next value until end of array is reached
-
-// re-factor to reduce complexity
-
 function productOfOthers(nums) {
-  let validNums = [];
-  let productsArray = [];
+  let productOfAllExceptIndex = [];
   let product = 1;
   for (let i = 0; i < nums.length; i++) {
-    for (let k = 0; k < nums.length; k++) {
-      if (nums[k] !== nums[i]) {
-        validNums.push(nums[k]);
-      }
-    }
-    for (let j = 0; j < validNums.length; j++) {
-      product *= validNums[j];
-    }
-    productsArray.push(product);
-    validNums = [];
-    product = 1;
+    productOfAllExceptIndex[i] = product;
+    product *= nums[i];
   }
-  console.log(productsArray);
+  product = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    productOfAllExceptIndex[i] *= product;
+    product *= nums[i];
+  }
+  return productOfAllExceptIndex;
 }
 
-productOfOthers([1, 7, 0, 4]); // should return [84, 12, 28, 21]
+productOfOthers([1, 7, 3, 4]); // should return [84, 12, 28, 21]
